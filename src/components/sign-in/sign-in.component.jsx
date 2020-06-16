9fc016bdfb1bd24370ea547ba40e2a62f5662a50
athena <longathena1995@gmail.com>
2020-06-16 11:25:38 +0700

    updating sign up component to shop app

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in.styles.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import {
+  signInWithGoogle
+} from "../../firebase/firebase.utils";
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,29 @@ const config = {
     appId: "1:849168160524:web:3341de8702d586f121db29",
     measurementId: "G-0B8F5GNMS7"
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
 
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch(err){
+            console.log('error creating user:'+err.message)
+        }
+    }
+    return userRef;
+}
 firebase.initializeApp(config)
 
 export const auth = firebase.auth();
@@ -21,5 +43,9 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// const providerFacebook = new firebase.auth.FacebookAuthProvider();
+// providerFacebook.setCustomParameters({ prompt: 'select_account' });
+// export const signInWithFacebook = () => auth.signInWithPopup(providerFacebook);
 
 export default firebase;
